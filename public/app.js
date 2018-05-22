d6e2560ec27a8f5f5248183d458ff0c12afa838f
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-05-22 12:34:19 +0200

    [CLIENT] Button click function added

--- public/app.js
+++ public/app.js
@@ -1,25 +1,40 @@
 var socket;
 var joinButton;
-var buttons =  new Array(6);
+var startGameButton;
+var buttons = new Array(6);
+var canvasWidth = 600;
+var canvasHeight = 800;
+
 function setup() {
   //p5.js canvas
-  createCanvas(600, 600);
+  createCanvas(canvasWidth, canvasHeight);
   background(0);
-
-  for(x = 0; x < 4 ; x++){
-    //4 players
-    for (var i = 0; i < buttons.length; i++) {
-     buttons[i] = createButton('pos' + i);
-     buttons[i].position(i*100  , x * 100);
-   }
-}
-
-
+  createButtons();
+  startGameButton = createButton('Start Game');
+  startGameButton.position(canvasWidth / 2, canvasHeight - 100);
+  console.log(canvasWidth + " x " + canvasHeight);
 
 }
+
 function draw() {
   //p5 function
 
+}
+
+function testFunction() {
+  console.log("button pressed" + buttonName);
+}
+
+
+function createButtons() {
+  for (x = 0; x < 4; x++) {
+    //4 players
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i] = createButton('pos' + i);
+      buttons[i].position(i * 100 + 50, x * 100 + 50);
+      buttons[i].mousePressed(testFunction);
+    }
+  }
 }
 
 function joinButtonPressed() {
